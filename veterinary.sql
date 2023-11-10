e4114fcf138ec6f31dd49fa40080aaed01d79841
Ramana1-982654 <supermivz@gmail.com>
2023-11-10 09:39:39 +0800

    feat/modify-lastname-dr-reyes-gonzales

--- veterinary.sql
+++ veterinary.sql
@@ -149,3 +149,7 @@ WHERE appointid =(SELECT appointid
 FROM appointments 
 WHERE animalid = (SELECT animalid 
 FROM animals WHERE name = 'Simba'));
+
+UPDATE doctors
+SET dlastname = 'Reyes-Gonzales'
+WHERE dfirstname = 'Dr. Sofia';
